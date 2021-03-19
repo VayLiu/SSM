ddd51bb2d2262c39ab119567a650160f2ebcb72b
VayLiu <vayliu2000@gmail.com>
2021-03-19 14:27:38 +0800

    创建测试类

---
+++ ch4/src/test/java/com/vayliu/aspectj/AnnotationAspectJTest.java
@@ -0,0 +1,24 @@
+package com.vayliu.aspectj;
+
+import com.vayliu.dynamic.jdk.TestDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * aspect中jXML配置的测试类
+ *
+ * @author: VayLiu
+ **/
+
+
+public class AnnotationAspectJTest {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("aspectjAnnotationContext.xml");
+        // 从容器中获取增强后的目标对象
+        TestDao testDaoAdvice = (TestDao) applicationContext.getBean("testDao");
+        // 执行方法
+        testDaoAdvice.save();
+    }
+
+}
